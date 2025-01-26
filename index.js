eea152925b76411602c83df10539a9924be1bb10
MichaelJackson-a <michaeljacksonsol12@gmail.com>
2025-01-25 23:07:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const btntelegram = document.getElementById("btntelegram");
+const btnx = document.getElementById("btnx");
+
+btntelegram.addEventListener("click", () => {
+  window.location.href = "https://t.me/MichaelJacksonsol1";
+});
+
+btnx.addEventListener("click", () => {
+  window.location.href = "https://x.com/ML_Jacksonsol";
+});
